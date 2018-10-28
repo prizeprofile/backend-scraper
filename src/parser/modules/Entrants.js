9723654198052c3f705a505b6a31ce3670f79bf6
Michael Bausano <michael@joinpouch.com>
2018-10-28 23:10:36 +0000

    CS

--- src/parser/modules/Entrants.js
+++ src/parser/modules/Entrants.js
@@ -5,7 +5,6 @@ module.exports = class Entrants extends ParserModule {
    * @inheritdoc
    */
   run () {
-    const promoter = this.competition.resolve('data').promoter
     const resource = this.competition.resolve('data').resource
     const methods = this.competition.resolve('entry_methods')
     let entrants = 0
